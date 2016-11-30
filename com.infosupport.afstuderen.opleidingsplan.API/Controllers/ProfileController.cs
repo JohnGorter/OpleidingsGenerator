bc76d31d655c82a251b9f57eb1926bae8941be59
I288975 <p.verheij@student.fontys.nl>
2016-11-30 15:43:03 +0100

    Enable CORS

--- com.infosupport.afstuderen.opleidingsplan.API/Controllers/ProfileController.cs
+++ com.infosupport.afstuderen.opleidingsplan.API/Controllers/ProfileController.cs
@@ -8,9 +8,11 @@ using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace com.infosupport.afstuderen.opleidingsplan.api.Controllers
 {
+    [EnableCors("*", "*", "*")]
     public class ProfileController : ApiController
     {
         private IAdministrationManager _administrationManager;
