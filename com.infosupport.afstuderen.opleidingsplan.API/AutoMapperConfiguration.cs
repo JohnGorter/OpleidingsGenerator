ab861598b44c8ee64422cad624221b60a7ee6b26
I288975 <p.verheij@student.fontys.nl>
2016-11-30 21:10:35 +0100

    removed demo code in automapperconfig

--- com.infosupport.afstuderen.opleidingsplan.API/AutoMapperConfiguration.cs
+++ com.infosupport.afstuderen.opleidingsplan.API/AutoMapperConfiguration.cs
@@ -16,22 +16,7 @@ namespace com.infosupport.afstuderen.opleidingsplan.api
                 mapper.CreateMap<integration.Coursesummary, model.CourseSummary>();
                 mapper.CreateMap<integration.CourseImplementation, model.CourseImplementation>();
                 mapper.CreateMap<Models.RestEducationPlan, generator.EducationPlanData>();
-  
-                
-                //ONLY FOR DEMO
-                mapper.CreateMap<integration.Course, model.EducationPlanCourse>()
-                    .ForMember(dest => dest.Date, opt => opt.MapFrom(src => RandomDay()))
-                    .ForMember(dest => dest.Days, opt => opt.MapFrom(src => null != src.CourseImplementations.FirstOrDefault() ? src.CourseImplementations.FirstOrDefault().Days.Count() : 4))
-                    .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-                    .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price));
             });
-        }
-
-        private static Random gen = new Random();
-        static DateTime RandomDay()
-        {
-            DateTime start = DateTime.Now;
-            return start.AddDays(gen.Next(150));
         }
     }
 }
